a59949025c12ba970027bc07271ad4d7e28d151a
yurio <yurio@ua.fm>
2014-02-26 20:58:12 +0200

    BAP-3164: Update migrations, fix installer

--- Migrations/Schema/v1_1/OroWorkflowBundle.php
+++ Migrations/Schema/v1_1/OroWorkflowBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\WorkflowBundle\Migrations\Schemas\v1_1;
+namespace Oro\Bundle\WorkflowBundle\Migrations\Schema\v1_1;
 
 use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\InstallerBundle\Migrations\Migration;
